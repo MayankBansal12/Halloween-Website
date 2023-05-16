e134b0a56df24c07f3050795e7662f62ef921c50
Mayank <mayankbansal125@gmail.com>
2023-05-17 01:43:28 +0530

    Style Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu=document.querySelector(".nav-menu");
+const navToggle=document.querySelector(".nav-toggle");
+const navClose=document.querySelector(".close-btn");
 
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.add("show-menu");
+})
+navClose.addEventListener("click",()=>{
+    navMenu.classList.remove("show-menu");
+})
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navList=document.querySelectorAll(".nav-list");
+function LinkAction(){
+    const navMenu=document.querySelector(".nav-menu");
+    // Removing navMenu class when a new item is clicked
+    navMenu.classList.remove("show-menu");
+}
+navList.forEach(n=>n.addEventListener("click",LinkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
